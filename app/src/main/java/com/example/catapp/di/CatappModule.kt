34f92b1d77efeceb06ffd9b6bf3c0ef8d54469d6
orosepa <l.olga.podgornaya@gmail.com>
2022-08-09 17:23:46 +0300

    Add retrofit instance

--- app/src/main/java/com/example/catapp/di/CatappModule.kt
+++ app/src/main/java/com/example/catapp/di/CatappModule.kt
@@ -1,12 +1,23 @@
 package com.example.catapp.di
 
+import com.example.catapp.data.remote.CatApi
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object CatappModule {
 
+    @Singleton
+    @Provides
+    fun provideCatApi(): CatApi {
+        return Retrofit.Builder()
+            .baseUrl("https://api.thecatapi.com")
+            .build()
+            .create(CatApi::class.java)
+    }
 }
