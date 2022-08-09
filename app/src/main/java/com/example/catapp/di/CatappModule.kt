37ebcaf5f8fd97c6efbd2b8f3459557b625e6b09
orosepa <l.olga.podgornaya@gmail.com>
2022-08-09 20:19:05 +0300

    Add data repository provider

--- app/src/main/java/com/example/catapp/di/CatappModule.kt
+++ app/src/main/java/com/example/catapp/di/CatappModule.kt
@@ -1,11 +1,14 @@
 package com.example.catapp.di
 
 import com.example.catapp.data.remote.CatApi
+import com.example.catapp.data.repository.CatappRepositoryImpl
+import com.example.catapp.domain.repository.CatappRepository
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -17,7 +20,12 @@ object CatappModule {
     fun provideCatApi(): CatApi {
         return Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com")
+            .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(CatApi::class.java)
     }
+
+    @Singleton
+    @Provides
+    fun provideDataRepository(api: CatApi): CatappRepository = CatappRepositoryImpl(api)
 }
