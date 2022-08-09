ebb730af4797cd37fe005b41a19d7c700e31eefd
orosepa <l.olga.podgornaya@gmail.com>
2022-08-09 13:10:21 +0300

    Set up DI

--- app/src/main/java/com/example/catapp/ui/CatAppActivity.kt
+++ app/src/main/java/com/example/catapp/ui/CatAppActivity.kt
@@ -1,9 +1,12 @@
-package com.example.catapp
+package com.example.catapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.catapp.R
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class CatAppActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
