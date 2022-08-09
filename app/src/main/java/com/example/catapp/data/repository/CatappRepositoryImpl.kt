edef3b0dd4b38d40d9152b24f612311204e78355
orosepa <l.olga.podgornaya@gmail.com>
2022-08-09 17:32:05 +0300

    Create repository implementation

---
+++ app/src/main/java/com/example/catapp/data/repository/CatappRepositoryImpl.kt
@@ -0,0 +1,11 @@
+package com.example.catapp.data.repository
+
+import com.example.catapp.data.remote.CatApi
+import com.example.catapp.domain.repository.CatappRepository
+import javax.inject.Inject
+
+class CatappRepositoryImpl @Inject constructor (
+    private val api: CatApi
+) : CatappRepository {
+
+}
