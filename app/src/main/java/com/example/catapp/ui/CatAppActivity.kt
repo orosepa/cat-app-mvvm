723c98b5c51c5e3c64b005daeb32f2f09ff188f2
orosepa <l.olga.podgornaya@gmail.com>
2022-08-09 16:48:12 +0300

    Add binding and navcontroller

--- app/src/main/java/com/example/catapp/ui/CatAppActivity.kt
+++ app/src/main/java/com/example/catapp/ui/CatAppActivity.kt
@@ -2,13 +2,23 @@ package com.example.catapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.catapp.R
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.catapp.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class CatAppActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setupWithNavController(
+            binding.catappNavHostFragment.findNavController()
+        )
     }
 }
