90566d7368611c399106bf51755638391c0560df
orosepa <l.olga.podgornaya@gmail.com>
2022-08-09 20:20:44 +0300

    Add view binding

--- app/src/main/java/com/example/catapp/ui/fragments/BreedsFragment.kt
+++ app/src/main/java/com/example/catapp/ui/fragments/BreedsFragment.kt
@@ -1,7 +1,9 @@
 package com.example.catapp.ui.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.catapp.R
 import com.example.catapp.databinding.FragmentBreedsBinding
@@ -9,6 +11,15 @@ import com.example.catapp.databinding.FragmentBreedsBinding
 class BreedsFragment : Fragment(R.layout.fragment_breeds) {
 
     private lateinit var binding: FragmentBreedsBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentBreedsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
--- app/src/main/java/com/example/catapp/ui/fragments/LikedFragment.kt
+++ app/src/main/java/com/example/catapp/ui/fragments/LikedFragment.kt
@@ -1,14 +1,26 @@
 package com.example.catapp.ui.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.catapp.R
+import com.example.catapp.databinding.FragmentGalleryBinding
 import com.example.catapp.databinding.FragmentLikedBinding
 
 class LikedFragment : Fragment(R.layout.fragment_liked) {
 
     private lateinit var binding: FragmentLikedBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentLikedBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
