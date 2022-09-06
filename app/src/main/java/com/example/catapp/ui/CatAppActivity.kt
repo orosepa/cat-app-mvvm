894f82d943847eb5f3c50631f39682614200a70f
orosepa <l.olga.podgornaya@gmail.com>
2022-09-06 17:00:22 +0300

    Add animations

--- app/src/main/java/com/example/catapp/ui/CatAppActivity.kt
+++ app/src/main/java/com/example/catapp/ui/CatAppActivity.kt
@@ -1,25 +1,13 @@
 package com.example.catapp.ui
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.AttributeSet
-import android.util.Log
 import android.view.View
-import androidx.fragment.app.Fragment
-import androidx.fragment.app.commit
-import androidx.fragment.app.replace
 import androidx.navigation.NavController
-import androidx.navigation.NavDestination.Companion.hierarchy
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.catapp.R
 import com.example.catapp.databinding.ActivityMainBinding
-import com.example.catapp.ui.fragments.BreedsFragment
-import com.example.catapp.ui.fragments.OneBreedFragment
-import com.example.catapp.ui.viewmodels.GalleryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
