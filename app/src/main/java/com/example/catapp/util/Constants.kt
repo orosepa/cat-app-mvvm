8561cd2013c81f75d31e58ef8b869bd4efc81d41
orosepa <70800588+orosepa@users.noreply.github.com>
2022-10-04 18:47:28 +0300

    Update Constants.kt

--- app/src/main/java/com/example/catapp/util/Constants.kt
+++ app/src/main/java/com/example/catapp/util/Constants.kt
@@ -1,7 +1,6 @@
 package com.example.catapp.util
 
 object Constants {
-    const val CATS_API = "be557d71-0866-47b7-9a18-f45999217e13"
     const val QUERY_PAGE_SIZE = 20
     const val BASE_URL = "https://api.thecatapi.com/"
 }
